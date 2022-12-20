1897786a1adcf1a0466ee45c09cd0fcc26757a8c
Shimshir Emirdzan IU7-23B <shimshireao@student.bmstu.ru>
2022-12-20 10:04:29 +0300

    done all

--- lab_08/inc/graph.h
+++ lab_08/inc/graph.h
@@ -7,9 +7,27 @@ typedef struct
     int **data;
 } matrix_t;
 
+/*
+* Функция создает матрицу смежности
+*
+* Принимает количетсво вершин
+*/
 matrix_t *create_matrix(int count);
+
+/*
+* Функция выделяет память для матрицы смежности
+*
+* Принимает количетсво вершин
+*/
 int **allocate_matrix(int count);
+
+/*
+* Функция освобождает память для матрицы смежности
+*
+* Принимает количетсво вершин и матрицу
+*/
 void free_matrix(int **data, int count);
+
 int check_empty_matrix(matrix_t *graph);
 int check_graph_null(matrix_t *graph);
 
--- lab_08/inc/task.h
+++ lab_08/inc/task.h
@@ -4,6 +4,12 @@
 #include "graph.h"
 
 int find_dst_with_Dijkstra(matrix_t *graph);
+
+/*
+* Функция для вызова алгоритма Дейкстры
+*
+* Принимает матрицу, массив расстояний до вершин, начальная вершина
+*/
 void Dijkstra(matrix_t *graph, int distance[graph->count], int top);
 int get_top_and_way(int *top, int *min_way, matrix_t *graph);
 void time_get();
